1d361a55c04d3afac9c6a927e590c90d23e40775
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2023-01-12 23:21:29 +0100

    Add after

--- src/main/java/com/przemyslaw_zykubek/spring_first_project/aspects/loggerAspect.java
+++ src/main/java/com/przemyslaw_zykubek/spring_first_project/aspects/loggerAspect.java
@@ -3,6 +3,7 @@ package com.przemyslaw_zykubek.spring_first_project.aspects;
 import java.time.LocalDateTime;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +14,9 @@ public class loggerAspect {
     @Before("execution(* com.przemyslaw_zykubek.spring_first_project.controllers.*.*(..))")
     public void before(JoinPoint joinPoint) {
         System.out.println(LocalDateTime.now() + "Method" + joinPoint + " started");
+    }
+    @After("execution(* com.przemyslaw_zykubek.spring_first_project.controllers.*.*(..))")
+    public void after(JoinPoint joinPoint) {
+        System.out.println(LocalDateTime.now() + "Method" + joinPoint + " stopped");
     }
 }
