294593c15926bb978b8eb28a1131aaad144b35fb
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2023-01-12 23:18:17 +0100

    Add logger

---
+++ src/main/java/com/przemyslaw_zykubek/spring_first_project/aspects/loggerAspect.java
@@ -0,0 +1,17 @@
+package com.przemyslaw_zykubek.spring_first_project.aspects;
+
+import java.time.LocalDateTime;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class loggerAspect {
+    @Before("execution(* com.przemyslaw_zykubek.spring_first_project.controllers.*.*(..))")
+    public void before(JoinPoint joinPoint) {
+        System.out.println(LocalDateTime.now() + "Method" + joinPoint + " started");
+    }
+}
